bc8ebe34afb82645898744b3f133f9f54e1f6b47
sheephuman <shin.citysheep@gmail.com>
2024-04-22 10:28:03 +0900

    マルチバインディングを使用しない例

--- WPFMaltiBindingTest/MainWindow.xaml.cs
+++ WPFMaltiBindingTest/MainWindow.xaml.cs
@@ -8,48 +8,7 @@ namespace WPFMaltiBindingTest
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
         {
-            private string _userInput = string.Empty;
-
-
-
-            //ユーザー入力用文字列
-            public string UserInput
-            {
-                get => _userInput;
-                set
-                {
-                    if (_userInput != value)
-                    {
-                        _userInput = value;
-                        StringB = $"-b:v {_userInput}k";
-                        OnPropertyChanged(nameof(UserInput));
-                    }
-                }
-            }
-
-
-            // ビューモデルのプロパティを公開するためのプロパティ
-            private string _stringB { get; set; } = string.Empty;
-            // INotifyPropertyChangedのイベント
-            public event PropertyChangedEventHandler? PropertyChanged;
-
-            // StringBプロパティ
-            public string StringB
-            {
-                get { return _stringB; }
-                set
-                {
-              
-                    if (_stringB != value)
-                    {
-
-                        _stringB = value;
-                        OnPropertyChanged(nameof(StringB));  // 変更通知
-                   
-                    }
-                }
-            }
-
+         
 
             public MainWindow()
             {
@@ -58,10 +17,14 @@ namespace WPFMaltiBindingTest
 
 
                 // DataContextにこのウィンドウ自体をセット（コードビハインドプロパティ用）
-                DataContext = this;
+                var qf =new QueryField();
+                DataContext = qf;
             }
-            // プロパティが変更された時にUIに通知するためのヘルパーメソッド
-            protected void OnPropertyChanged(string propertyName)
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // プロパティが変更された時にUIに通知するためのヘルパーメソッド
+        protected void OnPropertyChanged(string propertyName)
             {
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
---
+++ WPFMaltiBindingTest/QueryField.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+
+namespace WPFMaltiBindingTest
+{
+    public class QueryField : INotifyPropertyChanged
+    {
+        private string _userInputA = string.Empty;
+        public string UserInputA
+        {
+            get => _userInputA;
+            set
+            {
+                if (_userInputA != value)
+                {
+                    _userInputA = value;
+                    OnPropertyChanged(nameof(UserInputA));
+                    UpdateAllInput();
+                }
+            }
+        }
+
+        private string _userInputB = string.Empty;
+        public string UserInputB
+        {
+            get => _userInputB;
+            set
+            {
+                if (_userInputB != value)
+                {
+                    _userInputB = value;
+                    OnPropertyChanged(nameof(UserInputB));
+                    UpdateAllInput();
+                }
+            }
+        }
+
+        private string _allInput = string.Empty;
+        public string AllInput
+        {
+            get => _allInput;
+            private set
+            {
+                if (_allInput != value)
+                {
+                    _allInput = value;
+                    OnPropertyChanged(nameof(AllInput));
+                }
+            }
+        }
+        private void UpdateAllInput()
+        {
+            AllInput = $"-b:v {_userInputA}k -codec:v {_userInputB}";
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
